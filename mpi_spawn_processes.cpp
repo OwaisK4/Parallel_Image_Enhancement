7a5697103f56f34f58cbb5a20c2538e667aa5c71
Owais Ali Khan <owaisalikhan2003@gmail.com>
2023-11-18 19:55:58 +0500

    Rewrote image conversion with openmp. Currently works by taking input no. of threads and using them to convert image to ppm using imagemagick in parallel.

--- mpi_spawn_processes.cpp
+++ mpi_spawn_processes.cpp
@@ -1,6 +1,5 @@
 #include <filesystem>
 #include <iostream>
-#include <mpi.h>
 #include <omp.h>
 #include <string>
 #include <vector>
@@ -8,43 +7,37 @@ namespace fs = std::filesystem;
 
 std::string path = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images";
 std::vector<std::string> files;
+double start, end, elapsed;
 
 int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    if (rank == 0) { // Master process
-        for (const auto &entry : fs::directory_iterator(path)) {
-            std::string file = entry.path().string();
-            if (file.find("ppm", 0) == file.npos)
-                files.push_back(file);
-        }
-        for (auto &it : files) {
-            std::cout << it << "\n";
-        }
-        std::cout << "Total no. of files: " << files.size() << "\n";
-
-        if (size == 1) {
-            for (auto &file : files) {
-                int index = file.find(".", 0);
-                std::string basename = file.substr(0, index);
-                std::string command = "convert '" + file + "' -compress none '" + basename + ".ppm'";
-                system(command.c_str());
-            }
-        }
-        //  else {
-        //     int slaves = size - 1;
-        //     int per_slave = files.size() / slaves;
-        //     for (int slave = 1; slave <= slaves; slave++) {
-        //     }
-
-        //     int remaining = files.size() - (slaves * per_slave);
-        // }
+    for (const auto &entry : fs::directory_iterator(path)) {
+        std::string file = entry.path().string();
+        if (file.find("ppm", 0) == file.npos)
+            files.push_back(file);
     }
+    // for (auto &it : files) {
+    //     std::cout << it << "\n";
+    // }
+    std::cout << "Total no. of files: " << files.size() << "\n";
 
-    MPI_Finalize();
+    int threads = 8;
+    std::cout << "Enter no. of threads to use for conversion: ";
+    std::cin >> threads;
+    threads = std::min(8, threads);
+    threads = std::max(1, threads);
+
+    start = omp_get_wtime();
+#pragma omp parallel for num_threads(threads)
+    for (int i = 0; i < files.size(); i++) {
+        std::string file = files[i];
+        int index = file.find(".", 0);
+        std::string basename = file.substr(0, index);
+        std::string command = "convert '" + file + "' -compress none '" + basename + ".ppm'";
+        system(command.c_str());
+    }
+    end = omp_get_wtime();
+    elapsed = end - start;
+    std::cout << "Time taken = " << elapsed << " seconds.\n";
 
     return 0;
 }
