f698505b3601398f8c968e1d03e1581e3dc6286c
Owais Ali Khan <owaisalikhan2003@gmail.com>
2023-11-20 00:53:31 +0500

    Gaussian blurring works. Still some artifacts from hsv conversion tho. Will write code to sharpen images next.

--- ppm_utils.hpp
+++ ppm_utils.hpp
@@ -1,9 +1,16 @@
+#include <algorithm>
 #include <cmath>
 #include <fstream>
 #include <iostream>
 #include <omp.h>
 #include <string>
 #include <vector>
+
+std::vector<std::vector<double>> kernel = {{1, 4, 6, 4, 1},
+                                           {4, 16, 24, 16, 4},
+                                           {6, 24, 36, 24, 6},
+                                           {4, 16, 24, 16, 4},
+                                           {1, 4, 6, 4, 1}};
 
 class Pixel {
 public:
@@ -60,54 +67,56 @@ std::ostream &operator<<(std::ostream &outputStream, const PPMObject &image) { /
     return outputStream;
 }
 
-void RGB_to_HSV(PPMObject &RGB_image, int threads) {
+std::vector<HSV> RGB_to_HSV(const PPMObject &RGB_image, int threads) {
     size_t size = RGB_image.width * RGB_image.height;
-#pragma omp parallel for num_threads(threads) shared(RGB_image, size)
+    std::vector<HSV> hsv(size);
+#pragma omp parallel for num_threads(threads) shared(RGB_image, hsv, size)
     for (int i = 0; i < size; i++) {
         double scaled_red = RGB_image.pixels[i].red / 255.0;
         double scaled_green = RGB_image.pixels[i].green / 255.0;
         double scaled_blue = RGB_image.pixels[i].blue / 255.0;
-        double c_min = std::min(scaled_red, std::min(scaled_green, scaled_blue));
-        double c_max = std::max(scaled_red, std::max(scaled_green, scaled_blue));
+        double c_max = std::max({scaled_red, scaled_green, scaled_blue});
+        double c_min = std::min({scaled_red, scaled_green, scaled_blue});
         double delta = c_max - c_min;
         double h, s, v;
         if (delta == 0) {
-            h = 0;
-            s = 0;
-            v = c_max * 255;
+            h = 0.0;
         } else {
             if (c_max == scaled_red) {
                 h = 60.0 * ((scaled_green - scaled_blue) / delta);
             } else if (c_max == scaled_green) {
-                h = 120.0 * ((scaled_blue - scaled_red) / delta);
+                h = 60.0 * ((scaled_blue - scaled_red) / delta) + 120.0;
             } else {
-                h = 60.0 * ((scaled_red - scaled_green) / delta);
+                h = 60.0 * ((scaled_red - scaled_green) / delta) + 240.0;
             }
-            if (h < 0)
-                h = h + 360;
-            h = ceil(h / 2);
+            h = std::fmod(h, 360.0);
+            if (h < 0.0)
+                h = h + 360.0;
+            // h = h / 2;
             if (c_max == 0)
                 s = 0;
             else
                 s = (delta / c_max) * 255.0;
-            v = c_max * 255;
-            RGB_image.pixels[i].red = h;
-            RGB_image.pixels[i].green = s;
-            RGB_image.pixels[i].blue = v;
+            v = c_max * 255.0;
+            hsv[i].hue = h;
+            hsv[i].saturation = s;
+            hsv[i].value = v;
         }
     }
+    return hsv;
 }
 
-void HSV_to_RGB(PPMObject &HSV_image, int threads) {
-    size_t size = HSV_image.width * HSV_image.height;
-#pragma omp parallel for num_threads(threads) shared(HSV_image, size)
+void HSV_to_RGB(PPMObject &image, std::vector<HSV> &hsv, int threads) {
+    size_t size = image.width * image.height;
+#pragma omp parallel for num_threads(threads) shared(image, hsv, size)
     for (int i = 0; i < size; i++) {
         double R, G, B;
-        int H = HSV_image.pixels[i].red * 2;
-        double S = HSV_image.pixels[i].green / 255.0;
-        double V = HSV_image.pixels[i].blue / 255.0;
+        double H = hsv[i].hue;
+        double S = hsv[i].saturation / 255.0;
+        double V = hsv[i].value / 255.0;
         double C = S * V;
-        double X = C * (1.0 - fabs(fmod(((float)H / 60.0), 2.0) - 1.0));
+        double X = C * (1.0 - std::fabs(std::fmod((H / 60.0), 2.0) - 1.0));
+        // std::cout << "X = " << X << "\n";
         double m = V - C;
         if (H >= 0 && H < 60) {
             R = C;
@@ -137,19 +146,79 @@ void HSV_to_RGB(PPMObject &HSV_image, int threads) {
         R = (R + m) * 255.0;
         G = (G + m) * 255.0;
         B = (B + m) * 255.0;
-        HSV_image.pixels[i].red = R;
-        HSV_image.pixels[i].green = G;
-        HSV_image.pixels[i].blue = B;
+        R = std::min(255.0, std::max(0.0, R));
+        G = std::min(255.0, std::max(0.0, G));
+        B = std::min(255.0, std::max(0.0, B));
+
+        image.pixels[i].red = R;
+        image.pixels[i].green = G;
+        image.pixels[i].blue = B;
     }
 }
 
-void RGB_to_Grayscale(PPMObject &RGB_image, int threads) {
+void BlurImage(PPMObject &image, std::vector<HSV> &hsv, int threads) {
+    std::vector<HSV> original(hsv);
+    // for (auto &it : kernel)
+    //     for (auto &jt : it)
+    //         jt = jt / 256;
+    double fIntens = 0, curIntens = 0;
+#pragma omp parallel for num_threads(threads) shared(image, hsv, original) private(fIntens, curIntens) collapse(2)
+    for (int i = 2; i < image.height - 2; i++) {
+        for (int j = 2; j < image.width - 2; j++) {
+            fIntens = 0;
+            for (int k = -2; k <= 2; k++) {
+                for (int l = -2; l <= 2; l++) {
+                    double I = original[((i + k) * image.width) + (j + l)].value;
+                    double H = kernel[k + 2][l + 2];
+                    curIntens = I * H;
+                    fIntens += curIntens;
+                }
+            }
+            hsv[(i * image.width) + j].value = fIntens / 256.0;
+        }
+    }
+}
+
+PPMObject RGB_to_Grayscale(const PPMObject &RGB_image, int threads) {
+    PPMObject grayscale_image(RGB_image);
     size_t size = RGB_image.width * RGB_image.height;
-#pragma omp parallel for num_threads(threads) shared(RGB_image, size)
+#pragma omp parallel for num_threads(threads) shared(RGB_image, grayscale_image, size)
     for (int i = 0; i < size; i++) {
         int grayVal = (RGB_image.pixels[i].red * 0.21) + (RGB_image.pixels[i].green * 0.72) + (RGB_image.pixels[i].blue * 0.07);
-        RGB_image.pixels[i].red = grayVal;
-        RGB_image.pixels[i].green = grayVal;
-        RGB_image.pixels[i].blue = grayVal;
+        grayscale_image.pixels[i].red = grayVal;
+        grayscale_image.pixels[i].green = grayVal;
+        grayscale_image.pixels[i].blue = grayVal;
     }
+    return grayscale_image;
+}
+
+PPMObject RGB_Extract_Red(const PPMObject &RGB_image, int threads) {
+    PPMObject red_image(RGB_image);
+    size_t size = RGB_image.width * RGB_image.height;
+#pragma omp parallel for num_threads(threads) shared(red_image, size)
+    for (int i = 0; i < size; i++) {
+        red_image.pixels[i].green = 0;
+        red_image.pixels[i].blue = 0;
+    }
+    return red_image;
+}
+PPMObject RGB_Extract_Green(const PPMObject &RGB_image, int threads) {
+    PPMObject green_image(RGB_image);
+    size_t size = RGB_image.width * RGB_image.height;
+#pragma omp parallel for num_threads(threads) shared(green_image, size)
+    for (int i = 0; i < size; i++) {
+        green_image.pixels[i].red = 0;
+        green_image.pixels[i].blue = 0;
+    }
+    return green_image;
+}
+PPMObject RGB_Extract_Blue(const PPMObject &RGB_image, int threads) {
+    PPMObject blue_image(RGB_image);
+    size_t size = RGB_image.width * RGB_image.height;
+#pragma omp parallel for num_threads(threads) shared(blue_image, size)
+    for (int i = 0; i < size; i++) {
+        blue_image.pixels[i].red = 0;
+        blue_image.pixels[i].green = 0;
+    }
+    return blue_image;
 }
--- testing_ppm.cpp
+++ testing_ppm.cpp
@@ -7,56 +7,75 @@
 #include <vector>
 using namespace std;
 
-// string dirpath = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images/";
-string infilepath = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images/mayuri_suoh.ppm";
-string rgbfilepath = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images/mayuri_suoh_rgb.ppm";
-string hsvfilepath = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images/mayuri_suoh_hsv.ppm";
-string gsfilepath = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images/mayuri_suoh_gs.ppm";
+string dirpath = "/home/owaisk4/Win_backup/FAST NU assignments/Parallel and Distributed Computing/Project/Image Processing/Code/Images/";
+string filename = "owais";
 double start_time, end_time, elapsed; // For calculating benchmarks
 
 int main() {
-    fstream infile(infilepath, ios::in);
-    fstream rgboutfile(rgbfilepath, ios::out);
-    fstream hsvoutfile(hsvfilepath, ios::out);
-    // fstream gsoutfile(gsfilepath, ios::out);
-    PPMObject ppm;
-
+    fstream infile(dirpath + filename + ".ppm", ios::in);
     if (infile.fail()) {
         cout << "Input file doesn't exist. Exiting.\n";
-        remove(rgbfilepath.c_str());
-        remove(hsvfilepath.c_str());
-        // remove(gsfilepath.c_str());
         return 0;
     }
+    fstream gsoutfile(dirpath + filename + "_grayscale.ppm", ios::out);
+    fstream redoutfile(dirpath + filename + "_rgb_red.ppm", ios::out);
+    fstream greenoutfile(dirpath + filename + "_rgb_green.ppm", ios::out);
+    fstream blueoutfile(dirpath + filename + "_rgb_blue.ppm", ios::out);
+    fstream bluroutfile(dirpath + filename + "_blurred.ppm", ios::out);
 
+    PPMObject ppm;
     infile >> ppm;
     cout << "Completed reading from file.\n";
 
-    // #pragma omp parallel for num_threads(omp_get_max_threads())
-    //     for (int i = 0; i < ppm.height * ppm.width; i++) {
-    //         ppm.pixels[i].green = 0;
-    //     }
-    // PPMObject hsv = RGB_to_HSV(ppm, 8);
-    // PPMObject rgb = HSV_to_RGB(hsv, 8);
-    RGB_to_HSV(ppm, 8);
-    hsvoutfile << ppm;
+    int threads;
+    cout << "Enter no. of threads to use for conversion: ";
+    cin >> threads;
+    threads = max(1, min(8, threads));
+    start_time = omp_get_wtime();
 
-    HSV_to_RGB(ppm, 8);
-    rgboutfile << ppm;
+    PPMObject grayscale = RGB_to_Grayscale(ppm, threads);
+    PPMObject red_ppm = RGB_Extract_Red(ppm, threads);
+    PPMObject green_ppm = RGB_Extract_Green(ppm, threads);
+    PPMObject blue_ppm = RGB_Extract_Blue(ppm, threads);
+    vector<HSV> hsv = RGB_to_HSV(ppm, threads);
+    BlurImage(ppm, hsv, threads);
+    HSV_to_RGB(ppm, hsv, threads);
 
-    // RGB_to_Grayscale(ppm, 8);
-    // gsoutfile << ppm;
+    end_time = omp_get_wtime();
+    elapsed = end_time - start_time;
+    cout << "Time taken = " << elapsed << " seconds.\n";
 
+#pragma omp parallel sections
+    {
+#pragma omp section
+        {
+            gsoutfile << grayscale;
+        }
+#pragma omp section
+        {
+            redoutfile << red_ppm;
+        }
+#pragma omp section
+        {
+            greenoutfile << green_ppm;
+        }
+#pragma omp section
+        {
+            blueoutfile << blue_ppm;
+        }
+#pragma omp section
+        {
+            bluroutfile << ppm;
+        }
+    }
     cout << "Completed writing to file.\n";
+
     infile.close();
-    rgboutfile.close();
-    hsvoutfile.close();
-    // gsoutfile.close();
+    bluroutfile.close();
+    gsoutfile.close();
+    redoutfile.close();
+    greenoutfile.close();
+    blueoutfile.close();
 
     return 0;
 }
-
-// start_time = omp_get_wtime();
-// end_time = omp_get_wtime();
-// elapsed = end_time - start_time;
-// cout << "Time taken = " << elapsed << " seconds.\n";
